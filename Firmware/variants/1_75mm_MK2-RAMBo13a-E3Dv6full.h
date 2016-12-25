ad5567480183d6e0525d971deea633a9a8ece38d
Aaron Yourk <ayourk@gmail.com>
2016-12-25 03:03:33 -0700

    Added PT100 bed PID values (copied from extruder PID and untested)

--- Firmware/variants/1_75mm_MK2-RAMBo13a-E3Dv6full.h
+++ Firmware/variants/1_75mm_MK2-RAMBo13a-E3Dv6full.h
@@ -243,9 +243,16 @@ BED SETTINGS
 #ifdef PIDTEMPBED
 //120v 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
 //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
+#if defined(E3D_PT100_BED_WITH_AMP) || defined(E3D_PT100_BED_NO_AMP)
+// Define PID constants for extruder with PT100
+#define  DEFAULT_bedKp 21.70
+#define  DEFAULT_bedKi 1.60
+#define  DEFAULT_bedKd 73.76
+#else
 #define  DEFAULT_bedKp 126.13
 #define  DEFAULT_bedKi 4.30
 #define  DEFAULT_bedKd 924.76
+#endif
 
 //120v 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
 //from pidautotune
