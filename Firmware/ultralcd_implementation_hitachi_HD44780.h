0c9f873462644d3c6a56765170a41215888d0017
3d-gussner <3d.gussner@gmail.com>
2017-04-21 11:06:14 +0200

    Changed PINDA temp calibration ...
    ...messages to be clearer for the user which temp is calibrated. Added
    some '(length=xx, lines=y)' to prevent errors during running the
    'langtool.pl' script. Also change in the 'ultra...' file the cursor
    position to fit translated messages.

--- Firmware/ultralcd_implementation_hitachi_HD44780.h
+++ Firmware/ultralcd_implementation_hitachi_HD44780.h
@@ -964,7 +964,7 @@ static void lcd_implementation_status_screen()
 				char progress[4];
 				lcd.setCursor(0, 3);
 				lcd_printPGM(MSG_TEMP_CALIBRATION);
-				lcd.setCursor(12, 3);
+				lcd.setCursor(15, 3);
 				sprintf(progress, "%d/6", custom_message_state);
 				lcd.print(progress);
 			}
