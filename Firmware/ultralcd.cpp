26bd8fc5235f0eea184afac0a911736fedd3daca
PavelSindler <sindlerpa@gmail.com>
2017-07-07 10:58:10 +0200

    reverting

--- Firmware/ultralcd.cpp
+++ Firmware/ultralcd.cpp
@@ -4462,8 +4462,10 @@ static bool lcd_selfcheck_axis_sg(char axis) {
 
 		current_position_init = st_get_position_mm(axis);
 		if (i < 1) {
-
-			current_position[axis] += (axis_length + margin);
+			current_position[axis] += margin;
+			plan_buffer_line(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], current_position[3], manual_feedrate[0] / 60, active_extruder);
+			st_synchronize();
+			current_position[axis] += axis_length;
 			plan_buffer_line(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], current_position[3], manual_feedrate[0] / 60, active_extruder);
 #ifdef HAVE_TMC2130_DRIVERS
 			tmc2130_home_enter(X_AXIS_MASK << axis);
