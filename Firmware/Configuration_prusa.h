6bd221ad3ba68ac7563e4d0ea26172142f40ba87
PavelSindler <sindlerpa@gmail.com>
2017-11-22 16:07:03 +0100

    Y distance between nozzle and PINDA corrected

--- Firmware/Configuration_prusa.h
+++ Firmware/Configuration_prusa.h
@@ -317,7 +317,7 @@ const bool Z_MIN_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define MESH_HOME_Z_SEARCH 5 //Z lift for homing, mesh bed leveling etc.
 
 #define X_PROBE_OFFSET_FROM_EXTRUDER 23     // Z probe to nozzle X offset: -left  +right
-#define Y_PROBE_OFFSET_FROM_EXTRUDER 9     // Z probe to nozzle Y offset: -front +behind
+#define Y_PROBE_OFFSET_FROM_EXTRUDER 5     // Z probe to nozzle Y offset: -front +behind
 #define Z_PROBE_OFFSET_FROM_EXTRUDER -0.4  // Z probe to nozzle Z offset: -below (always!)
 #endif
 
--- Firmware/mesh_bed_calibration.cpp
+++ Firmware/mesh_bed_calibration.cpp
@@ -56,11 +56,6 @@ const float bed_skew_angle_extreme = (0.25f * M_PI / 180.f);
 // Positions of the bed reference points in the machine coordinates, referenced to the P.I.N.D.A sensor.
 // The points are the following: center front, center right, center rear, center left.
 const float bed_ref_points_4[] PROGMEM = {
-	//115.f - BED_ZERO_REF_X,   8.4f - BED_ZERO_REF_Y,
-	//216.f - BED_ZERO_REF_X, 104.4f - BED_ZERO_REF_Y,
-	//115.f - BED_ZERO_REF_X, 200.4f - BED_ZERO_REF_Y,
-	//13.f - BED_ZERO_REF_X, 104.4f - BED_ZERO_REF_Y
-
 	13.f - BED_ZERO_REF_X,   8.4f - BED_ZERO_REF_Y,
 	221.f - BED_ZERO_REF_X,  8.4f - BED_ZERO_REF_Y,
 	221.f - BED_ZERO_REF_X, 200.4f - BED_ZERO_REF_Y,
