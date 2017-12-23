1a69c979bfbc0cd55b0c7384246cacb0a7119471
Robert Pelnar <robert.pelnar@seznam.cz>
2017-12-23 01:01:57 +0100

    Increase Z current (PFW-90).

--- Firmware/Configuration_prusa.h
+++ Firmware/Configuration_prusa.h
@@ -181,8 +181,8 @@ const bool Z_MIN_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 #define TMC2130_SG_THRS_E       3     // stallguard sensitivity for E axis
 
 //new settings is possible for vsense = 1, running current value > 31 set vsense to zero and shift both currents by 1 bit right (Z axis only)
-#define TMC2130_CURRENTS_H {13, 20, 20, 35}  // default holding currents for all axes
-#define TMC2130_CURRENTS_R {13, 20, 20, 35}  // default running currents for all axes
+#define TMC2130_CURRENTS_H {13, 20, 25, 35}  // default holding currents for all axes
+#define TMC2130_CURRENTS_R {13, 20, 25, 35}  // default running currents for all axes
 
 //#define TMC2130_DEBUG
 //#define TMC2130_DEBUG_WR
