763bbc25319140e2f526f9ef40f6e379875bd960
Michal Průša <dj@michal-prusa.eu>
2017-12-27 19:41:58 +0100

    2nd USART -> rPi port

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -9,7 +9,7 @@
 
 // Firmware version
 #define FW_version "3.1.1-RC3"
-#define FW_build   138
+#define FW_build   138m1
 //#define FW_build   --BUILD-NUMBER--
 #define FW_version_build FW_version " b" STR(FW_build)
 
