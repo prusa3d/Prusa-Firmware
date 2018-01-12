3e6d853364981c82c3e79b9fabc38f354985869c
bubnikv <bubnikv@gmail.com>
2018-01-12 20:51:14 +0100

    Debugging of the stepper timer routine. When enabled through DEBUG_STEPPER_TIMER_MISSED, the printer is halted on stepper timer overflow and an error message is displayed.

--- Firmware/Configuration_prusa.h
+++ Firmware/Configuration_prusa.h
@@ -127,6 +127,7 @@ const bool Z_MIN_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 //#define DEBUG_DISABLE_FANCHECK     //disable fan check (no ISR INT7, check disabled)
 //#define DEBUG_DISABLE_FSENSORCHECK //disable fsensor check (no ISR INT7, check disabled)
 #define DEBUG_DUMP_TO_2ND_SERIAL   //dump received characters to 2nd serial line
+//#define DEBUG_STEPPER_TIMER_MISSED // Stop on stepper timer overflow, beep and display a message.
 #endif
 
 
