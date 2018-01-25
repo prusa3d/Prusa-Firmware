40f7629460be7bec7ebee98384ad1b847f7949f5
PavelSindler <sindlerpa@gmail.com>
2018-01-25 19:06:17 +0100

    test version for setting current

--- Firmware/Marlin_main.cpp
+++ Firmware/Marlin_main.cpp
@@ -272,6 +272,8 @@ int extruder_multiply[EXTRUDERS] = {100
   #endif
 };
 
+uint8_t tmc2130_current_unload = 35;
+
 int bowden_length[4] = {385, 385, 385, 385};
 
 bool is_usb_printing = false;
@@ -2202,7 +2204,7 @@ void gcode_M701()
 	custom_message_type = 2;
 
 	lcd_setstatuspgm(MSG_LOADING_FILAMENT);
-	current_position[E_AXIS] += 70;
+	current_position[E_AXIS] += 42;
 	plan_buffer_line(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], current_position[E_AXIS], 400 / 60, active_extruder); //fast sequence
 
 	current_position[E_AXIS] += 25;
@@ -2608,6 +2610,10 @@ void process_commands()
         prepare_arc_move(false);
       }
       break;
+	case 199:
+		if(code_seen('S')) tmc2130_current_unload = code_value();
+		
+		break;
     case 4: // G4 dwell      
       codenum = 0;
       if(code_seen('P')) codenum = code_value(); // milliseconds to wait
@@ -6126,19 +6132,39 @@ case 404:  //M404 Enter the nominal filament width (3mm, 1.75mm ) N<3.0> or disp
 		custom_message = true;
 		custom_message_type = 2;
 		lcd_setstatuspgm(MSG_UNLOADING_FILAMENT); 
-
+		//uint8_t tmc2130_mres_e = tmc2130_calc_mres(TMC2130_USTEPS_E);
+		uint8_t E_holding_bckp = tmc2130_current_h[E_AXIS];
+		uint8_t E_running_bckp = tmc2130_current_r[E_AXIS];
+		tmc2130_print_currents();
 //		extr_unload2();
 
-		current_position[E_AXIS] -= 45;
+		current_position[E_AXIS] += 4;
+		plan_buffer_line(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], current_position[E_AXIS], 2600 / 60, active_extruder);
+		current_position[E_AXIS] += 2;
+		plan_buffer_line(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], current_position[E_AXIS], 3600 / 60, active_extruder);
+		current_position[E_AXIS] += 3;
+		plan_buffer_line(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], current_position[E_AXIS], 4400 / 60, active_extruder);
+		current_position[E_AXIS] += 3;
 		plan_buffer_line(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], current_position[E_AXIS], 5200 / 60, active_extruder);
-        st_synchronize();
-        current_position[E_AXIS] -= 15;
-        plan_buffer_line(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], current_position[E_AXIS], 1000 / 60, active_extruder);
-        st_synchronize();
-        current_position[E_AXIS] -= 20;
-        plan_buffer_line(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], current_position[E_AXIS], 1000 / 60, active_extruder);
+
+		st_synchronize();
+		tmc2130_set_current_h(E_AXIS, tmc2130_current_unload);
+		tmc2130_set_current_r(E_AXIS, tmc2130_current_unload);
+		tmc2130_print_currents();
+
+		current_position[E_AXIS] -= 15;
+		plan_buffer_line(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], current_position[E_AXIS], 5800 / 60, active_extruder);
+		current_position[E_AXIS] -= 55;
+		plan_buffer_line(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], current_position[E_AXIS], 3600 / 60, active_extruder);
+		current_position[E_AXIS] -= 10;
+		plan_buffer_line(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], current_position[E_AXIS], 800 / 60, active_extruder);
 		st_synchronize();
 
+		tmc2130_set_current_h(E_AXIS, E_holding_bckp);
+		tmc2130_set_current_r(E_AXIS, E_running_bckp);
+
+		//tmc2130_setup_chopper(E_AXIS, 0b0011, E_holding_bckp, E_running_bckp);
+		tmc2130_print_currents();
 		lcd_display_message_fullscreen_P(MSG_PULL_OUT_FILAMENT);
 
 		//disable extruder steppers so filament can be removed
