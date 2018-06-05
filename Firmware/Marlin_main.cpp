7cc8b743c2108aee3fa09d9d1dedc7985c225b86
Marek Bel <marek.bel@posta.cz>
2018-06-05 18:27:36 +0200

    Add .gitignore. Setup doxyfile. Create project documentation main page. Document property of static object constructors on gcc-avr.

--- Firmware/Marlin_main.cpp
+++ Firmware/Marlin_main.cpp
@@ -1,30 +1,46 @@
 /* -*- c++ -*- */
-
-/*
-    Reprap firmware based on Sprinter and grbl.
- Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * @file
  */
 
-/*
- This firmware is a mashup between Sprinter and grbl.
-  (https://github.com/kliment/Sprinter)
-  (https://github.com/simen/grbl/tree)
-
- It has preliminary support for Matthew Roberts advance algorithm
-    http://reprap.org/pipermail/reprap-dev/2011-May/003323.html
+/**
+ * @mainpage Reprap 3D printer firmware based on Sprinter and grbl.
+ *
+ * @section intro_sec Introduction
+ *
+ * This firmware is a mashup between Sprinter and grbl.
+ * https://github.com/kliment/Sprinter
+ * https://github.com/simen/grbl/tree
+ *
+ * It has preliminary support for Matthew Roberts advance algorithm
+ * http://reprap.org/pipermail/reprap-dev/2011-May/003323.html
+ *
+ * Prusa Research s.r.o. https://www.prusa3d.cz
+ *
+ * @section copyright_sec Copyright
+ *
+ * Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @section notes_sec Notes
+ *
+ * * Do not create static objects in global functions.
+ *   Otherwise constructor guard against concurrent calls is generated costing
+ *   about 8B RAM and 14B flash.
+ *
+ *
  */
 
 #include "Marlin.h"
--- Firmware/Timer.h
+++ Firmware/Timer.h
@@ -1,4 +1,4 @@
-/*
+/**
  * @file
  * @author Marek Bel
  */
