67356ce35669c714f4d805a05ea90363c4c609db
Marek Bel <marek.bel@posta.cz>
2018-06-11 22:50:52 +0200

    Incomplete Timer test.

---
+++ Tests/Timer_test.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file
+ * @author Marek Bel
+ */
+
+
+#include "catch.hpp"
+#include "../Firmware/Timer.h"
+
+unsigned long millis()
+{
+    return 1;
+}
+
+TEST_CASE( "LongTimer tested.", "[timer]" )
+{
+    LongTimer timer;
+    REQUIRE( timer.running() == false);
+
+    timer.start();
+    REQUIRE( timer.running() == true);
+
+    timer.stop();
+    REQUIRE( timer.running() == false);
+
+    timer.start();
+    REQUIRE( timer.expired(0) == true );
+    REQUIRE( timer.expired(0) == false );
+    REQUIRE( timer.running() == false);
+
+    timer.start();
+    REQUIRE( timer.expired(1) == false );
+    REQUIRE( timer.running() == true);
+}
