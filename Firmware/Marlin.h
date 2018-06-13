1077895fd4ea65a8eb8db4405417653c30841bce
PavelSindler <sindlerpa@gmail.com>
2018-06-13 18:22:58 +0200

    Don't return "Not SD printing" in case that SD card print is paused

--- Firmware/Marlin.h
+++ Firmware/Marlin.h
@@ -357,7 +357,7 @@ extern uint8_t print_percent_done_silent;
 extern uint16_t print_time_remaining_silent;
 #define PRINT_TIME_REMAINING_INIT 65535
 #define PRINT_PERCENT_DONE_INIT 255
-#define PRINTER_ACTIVE (IS_SD_PRINTING || is_usb_printing || isPrintPaused || (custom_message_type == 4) || saved_printing || (lcd_commands_type == LCD_COMMAND_V2_CAL))
+#define PRINTER_ACTIVE (IS_SD_PRINTING || is_usb_printing || isPrintPaused || (custom_message_type == 4) || saved_printing || (lcd_commands_type == LCD_COMMAND_V2_CAL) || card.paused)
 
 extern void calculate_extruder_multipliers();
 
