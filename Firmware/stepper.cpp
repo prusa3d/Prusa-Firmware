f5ed37a88f4088304f8c9c27b24e0e99882af587
MRprusa3d <martin.remis@prusa3d.cz>
2018-07-30 10:52:31 +0200

    Sound
    "stepper.cpp" revert (debug mode only)

--- Firmware/stepper.cpp
+++ Firmware/stepper.cpp
@@ -26,7 +26,6 @@
 #include "planner.h"
 #include "temperature.h"
 #include "ultralcd.h"
-#include "sound.h"
 #include "language.h"
 #include "cardreader.h"
 #include "speed_lookuptable.h"
@@ -400,7 +399,6 @@ ISR(TIMER1_COMPA_vect) {
       stepper_timer_overflow_state = true;
       stepper_timer_overflow_last = TCNT1 - OCR1A;
       // Beep, the beeper will be cleared at the stepper_timer_overflow() called from the main thread.
-if((eSoundMode==e_SOUND_MODE_LOUD)||(eSoundMode==e_SOUND_MODE_ONCE))
       WRITE(BEEPER, HIGH);
     }
 #endif
