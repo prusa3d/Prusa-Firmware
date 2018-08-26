d3ed270a5a625a47bc568dd053f24bb0abc30614
PavelSindler <sindlerpa@gmail.com>
2018-08-26 23:52:45 +0200

    single material printer (no mmu unit) M600 hotfix: M600_check_state loop working

--- Firmware/Marlin_main.cpp
+++ Firmware/Marlin_main.cpp
@@ -8843,8 +8843,7 @@ void M600_check_state()
 {
 		//Wait for user to check the state
 		lcd_change_fil_state = 0;
-		
-		while ((lcd_change_fil_state == 0)||(lcd_change_fil_state != 1)){
+		while (lcd_change_fil_state != 1){
 			lcd_change_fil_state = 0;
 			KEEPALIVE_STATE(PAUSED_FOR_USER);
 			lcd_alright();
@@ -8871,7 +8870,7 @@ void M600_check_state()
 					lcd_update_enable(true);
 					break;
 			}
-	}
+		}
 }
 
 void M600_wait_for_user() {
@@ -9036,7 +9035,7 @@ if((eSoundMode==e_SOUND_MODE_LOUD)||(eSoundMode==e_SOUND_MODE_ONCE))
 			fsensor_disable();
 	}
 #endif //FILAMENT_SENSOR
-
+	lcd_update_enable(false);
 }
 
 #define FIL_LOAD_LENGTH 60
