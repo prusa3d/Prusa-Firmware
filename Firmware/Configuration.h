45066ebddc59a671ab778299774d7ccd38e77810
PavelSindler <sindlerpa@gmail.com>
2018-09-12 09:37:16 +0200

    lang fix

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -8,7 +8,7 @@
 
 // Firmware version
 #define FW_VERSION "3.4.0"
-#define FW_COMMIT_NR   1325
+#define FW_COMMIT_NR   1327
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
 #define FW_DEV_VERSION FW_VERSION_UNKNOWN
