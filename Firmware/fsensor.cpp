4a52683ad1d55c1974576c25192830a982b6706d
TheZeroBeast <TheZeroBeast@me.com>
2018-10-29 13:07:07 +1000

    Updated fsensor_check_autoload(

--- Firmware/fsensor.cpp
+++ Firmware/fsensor.cpp
@@ -286,10 +286,10 @@ bool fsensor_check_autoload(void)
 	{
 		puts_P(_N("fsensor_check_autoload = true !!!\n"));
 		if (mmu_enabled) {
-      mmu_puts_P(PSTR("FS\n"));
-      mmuFilamentLoading = false;
-      fsensor_autoload_check_stop();
-		  //mmuFilamentLoadSeen = true;
+      			mmu_puts_P(PSTR("FS\n"));
+      			mmuFilamentLoading = false;
+      			fsensor_autoload_check_stop();
+			mmuFilamentLoadSeen = true;
 		} else return true;
 	}
 	return false;
