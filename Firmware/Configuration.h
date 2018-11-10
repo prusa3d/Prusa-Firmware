20e854d3368d431c9b3c5807e62230472e303c90
Rob McKenzie <TheZeroBeast@me.com>
2018-11-11 09:18:55 +1000

    premade hex included

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -8,7 +8,7 @@
 
 // Firmware version
 #define FW_VERSION "5.0.1"
-#define FW_COMMIT_NR   2131
+#define FW_COMMIT_NR   2132
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
 #define FW_DEV_VERSION FW_VERSION_UNKNOWN
