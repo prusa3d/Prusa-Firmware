f1b32ea9e9e218b74fd2a05551b6c84d6e210375
Rob McKenzie <TheZeroBeast@me.com>
2018-11-15 16:10:59 +1000

    hex & version updated

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -8,7 +8,7 @@
 
 // Firmware version
 #define FW_VERSION "5.0.2-TZB"
-#define FW_COMMIT_NR   2210
+#define FW_COMMIT_NR   2234
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
 #define FW_DEV_VERSION FW_VERSION_UNKNOWN
