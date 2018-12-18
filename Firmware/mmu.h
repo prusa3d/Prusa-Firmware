c782373f2d87e7894b192fa370176242edd47287
PavelSindler <sindlerpa@gmail.com>
2018-12-18 16:09:10 +0100

    comment

--- Firmware/mmu.h
+++ Firmware/mmu.h
@@ -23,7 +23,7 @@ extern int16_t mmu_buildnr;
 #define MMU_TCODE_MOVE 3
 
 #define MMU_LOAD_FEEDRATE 19.02f //mm/s
-#define MMU_LOAD_TIME_MS 2000 //1000ms is load time for shortest allowed PTFE tubing and maximum loading speed
+#define MMU_LOAD_TIME_MS 2000 //should be fine tuned to load time for shortest allowed PTFE tubing and maximum loading speed
 
 #define MMU_CMD_NONE 0
 #define MMU_CMD_T0   0x10
