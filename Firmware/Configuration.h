acbfe89f0468bfc797c2e35df9ce1747cbdb01ba
PavelSindler <sindlerpa@gmail.com>
2018-12-19 20:07:18 +0100

    version changed

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -7,8 +7,8 @@
 #define STR(x) STR_HELPER(x)
 
 // Firmware version
-#define FW_VERSION "3.5.0"
-#define FW_COMMIT_NR   1749
+#define FW_VERSION "3.5.1"
+#define FW_COMMIT_NR   1778
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
 #define FW_DEV_VERSION FW_VERSION_UNKNOWN
