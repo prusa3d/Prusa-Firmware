60acdd23c5ac91696c9466763c53953d68d4b88d
PavelSindler <sindlerpa@gmail.com>
2019-01-28 21:12:38 +0100

    variants

--- Firmware/printers.h
+++ Firmware/printers.h
@@ -8,7 +8,9 @@
 #define PRINTER_MK2_SNMM    201
 #define PRINTER_MK25        250
 #define PRINTER_MK25_SNMM   251
+#define PRINTER_MK25S		252
 #define PRINTER_MK3         300
 #define PRINTER_MK3_SNMM    301
+#define PRINTER_MK3S		302
 
 #endif //PRINTERS_H
