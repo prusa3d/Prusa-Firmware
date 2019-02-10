e73c373a6c69117ade1001badf9b55450bba3391
3d-gussner <3d.gussner@gmail.com>
2019-02-10 15:30:55 +0100

    Revert Test FW_VERSION

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -7,7 +7,7 @@
 #define STR(x) STR_HELPER(x)
 
 // Firmware version
-#define FW_VERSION "3.5.2"
+#define FW_VERSION "3.5.2-RC1"
 #define FW_COMMIT_NR   1993
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
