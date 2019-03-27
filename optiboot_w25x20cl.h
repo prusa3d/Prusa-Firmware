ab11a8cada75cdeaf965107458269896a983eab3
cimoalpacino <cimoalpacino@users.noreply.github.com>
2019-03-27 20:26:46 +0100

    Revert "ALPHA FW (compiled and successfully uploaded to RAMPS)"
    This reverts commit 740b35ca23490853c5f79f66ae6ccd4d4df80035.

--- optiboot_w25x20cl.h
+++
@@ -1,6 +0,0 @@
-#ifndef OPTIBOOT_W25X20CL_H
-#define OPTIBOOT_W25X20CL_H
-
-extern void optiboot_w25x20cl_enter();
-
-#endif /* OPTIBOOT_W25X20CL_H */
--- spi.c
+++
@@ -1,8 +0,0 @@
-//spi.c - hardware SPI
-//#ifdef __SPI
-
-#include "spi.h"
-#include <avr/io.h>
-
-
-//#endif //__SPI
