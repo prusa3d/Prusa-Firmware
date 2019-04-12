5cefb2c901243e2b8fc82430cad14b9219cd08c7
PavelSindler <sindlerpa@gmail.com>
2019-04-12 19:46:41 +0200

    Heatbed maxtemp reduced to 125 C (MK2)

--- Firmware/variants/1_75mm_MK1-RAMBo10a-E3Dv6full.h
+++ Firmware/variants/1_75mm_MK1-RAMBo10a-E3Dv6full.h
@@ -102,7 +102,7 @@ EXTRUDER SETTINGS
 #endif
 #define HEATER_1_MAXTEMP 305
 #define HEATER_2_MAXTEMP 305
-#define BED_MAXTEMP 150
+#define BED_MAXTEMP 125
 
 #if defined(E3D_PT100_EXTRUDER_WITH_AMP) || defined(E3D_PT100_EXTRUDER_NO_AMP)
 // Define PID constants for extruder with PT100
