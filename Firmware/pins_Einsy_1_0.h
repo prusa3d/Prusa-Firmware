326c061d8c0018e8be5b8c434998d5ecdc887cb9
DRracer <drracer@seznam.cz>
2019-05-27 18:27:24 +0200

    testing pin 5

--- Firmware/pins_Einsy_1_0.h
+++ Firmware/pins_Einsy_1_0.h
@@ -99,7 +99,8 @@
 
 //#define KILL_PIN            32
 
-//#define LCD_BL_PIN          5   //backlight control pin
+// LCD backlight pin may interfere with something, this is yet to be found out correctly
+#define LCD_BL_PIN          5   //backlight control pin
 #define BEEPER              84  // Beeper on AUX-4
 #define LCD_PINS_RS         82
 #define LCD_PINS_ENABLE     61 // !!! changed from 18 (EINY03)
