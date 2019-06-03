1952e0a18f6b8dc8451a5c540e295a737b7317c8
Marek Bel <marek.bel@posta.cz>
2019-06-03 15:59:38 +0200

    Update FW_version string to final.

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -5,7 +5,7 @@
 #include "Configuration_prusa.h"
 
 // Firmware version
-#define FW_version "3.2.2-RC1"
+#define FW_version "3.2.2"
 
 #define FW_PRUSA3D_MAGIC "PRUSA3DFW"
 #define FW_PRUSA3D_MAGIC_LEN 10
