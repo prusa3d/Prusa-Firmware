7189f44bcd8ba8fa4487db7c638c90c1daed201f
Marek Bel <marek.bel@posta.cz>
2019-07-10 17:40:33 +0200

    Remove commented out defines.

--- Firmware/printers.h
+++ Firmware/printers.h
@@ -3,17 +3,6 @@
 
 #define PRINTER_UNKNOWN 0
 
-/* original definition(s)
-#define PRINTER_MK1         100
-#define PRINTER_MK2         200
-#define PRINTER_MK2_SNMM    201
-#define PRINTER_MK25        250
-#define PRINTER_MK25_SNMM   251
-#define PRINTER_MK25S	   252
-#define PRINTER_MK3         300
-#define PRINTER_MK3_SNMM    301
-#define PRINTER_MK3S	   302
-*/
 // *** MK1
 #define PRINTER_MK1	               100
 #define PRINTER_MK1_NAME           "MK1"
