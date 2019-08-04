7cae064611c7dfae0b20df623d4095ac5b7c4f99
Yuri D'Elia <wavexx@thregr.org>
2019-08-04 18:39:28 +0200

    Update EEPROM_LAST_ITEM

--- Firmware/eeprom.h
+++ Firmware/eeprom.h
@@ -206,7 +206,7 @@ static Sheets * const EEPROM_Sheets_base = (Sheets*)(EEPROM_SHEETS_BASE);
 #define EEPROM_UVLO_LA_K (EEPROM_SHEETS_BASE-4) // float
 
 //This is supposed to point to last item to allow EEPROM overrun check. Please update when adding new items.
-#define EEPROM_LAST_ITEM EEPROM_SHEETS_BASE
+#define EEPROM_LAST_ITEM EEPROM_UVLO_LA_K
 // !!!!!
 // !!!!! this is end of EEPROM section ... all updates MUST BE inserted before this mark !!!!!
 // !!!!!
