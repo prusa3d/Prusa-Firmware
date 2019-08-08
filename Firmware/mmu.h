c8015143137a785eff2a119f55ca8b6d69ab985b
Nikolai <nrinas@cube4fun.net>
2019-08-08 14:46:59 -0700

    Update mmu.h
    MMU unload time is different to load time.

--- Firmware/mmu.h
+++ Firmware/mmu.h
@@ -30,6 +30,8 @@ extern uint16_t mmu_power_failures;
 
 #define MMU_LOAD_FEEDRATE 19.02f //mm/s
 #define MMU_LOAD_TIME_MS 2000 //should be fine tuned to load time for shortest allowed PTFE tubing and maximum loading speed
+#define MMU_UNLOAD_TIME_MS 4000 //should be fine tuned to load time for shortest allowed PTFE tubing and maximum uloading speed
+
 
 enum class MmuCmd : uint_least8_t
 {
