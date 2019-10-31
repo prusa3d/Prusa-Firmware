c899320590b5b9e41181026076697747ad898a5a
3d-gussner <3d.gussner@gmail.com>
2019-10-31 15:08:07 +0100

    Fix M81 Turn off Power Supply... ... as it should be active if PS_ON_PIN is defined and assinged to an actual pin.

--- Firmware/Marlin_main.cpp
+++ Firmware/Marlin_main.cpp
@@ -6165,7 +6165,6 @@ Sigma_Exit:
           LCD_MESSAGERPGM(_T(WELCOME_MSG));
           lcd_update(0);
         break;
-      #endif
 
       //! ### M81 - Turn off Power Supply
       // --------------------------------------
@@ -6189,6 +6188,7 @@ Sigma_Exit:
         LCD_MESSAGERPGM(CAT4(CUSTOM_MENDEL_NAME,PSTR(" "),MSG_OFF,PSTR(".")));
         lcd_update(0);
 	  break;
+    #endif
 
     //! ### M82 - Set E axis to absolute mode
     // ---------------------------------------
