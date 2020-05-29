9f39fcc283d705270dcc41f305f149387ebcbc72
hergtoler <40492564+hergtoler@users.noreply.github.com>
2020-05-29 08:40:36 -0400

    Add filament change serial message

--- Firmware/Marlin_main.cpp
+++ Firmware/Marlin_main.cpp
@@ -7886,6 +7886,8 @@ Sigma_Exit:
 		if (mmu_enabled && code_seen("AUTO"))
 			automatic = true;
 
+		SERIAL_PROTOCOLLNPGM("Filament change - M600");
+
 		gcode_M600(automatic, x_position, y_position, z_shift, e_shift_init, e_shift_late);
 	
 	}
