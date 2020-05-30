4db0b12c22391e113c62220aca2590337a72e40a
3d-gussner <3d.gussner@gmail.com>
2020-05-30 17:16:02 +0200

    revert doxygen snytax

--- Firmware/fsensor.cpp
+++ Firmware/fsensor.cpp
@@ -731,25 +731,25 @@ void fsensor_update(void)
 }
 
 #ifdef IR_SENSOR_ANALOG
-//! This is called only upon start of the printer or when switching the fsensor ON in the menu
-//! We cannot do temporal window checks here (aka the voltage has been in some range for a period of time)
+/// This is called only upon start of the printer or when switching the fsensor ON in the menu
+/// We cannot do temporal window checks here (aka the voltage has been in some range for a period of time)
 bool fsensor_IR_check(){
 	if( IRsensor_Lmax_TRESHOLD <= current_voltage_raw_IR && current_voltage_raw_IR <= IRsensor_Hmin_TRESHOLD ){
-		//! If the voltage is in forbidden range, the fsensor is ok, but the lever is mounted improperly.
-		//! Or the user is so creative so that he can hold a piece of fillament in the hole in such a genius way,
-		//! that the IR fsensor reading is within 1.5 and 3V ... this would have been highly unusual
-		//! and would have been considered more like a sabotage than normal printer operation
+		/// If the voltage is in forbidden range, the fsensor is ok, but the lever is mounted improperly.
+		/// Or the user is so creative so that he can hold a piece of fillament in the hole in such a genius way,
+		/// that the IR fsensor reading is within 1.5 and 3V ... this would have been highly unusual
+		/// and would have been considered more like a sabotage than normal printer operation
 		printf_P(PSTR("fsensor in forbidden range 1.5-3V - check sensor\n"));
 		return false; 
 	}
 	
 	if( oFsensorPCB == ClFsensorPCB::_Rev04 ){
-		//! newer IR sensor cannot normally produce 4.6-5V, this is considered a failure/bad mount
+		/// newer IR sensor cannot normally produce 4.6-5V, this is considered a failure/bad mount
 		if( IRsensor_Hopen_TRESHOLD <= current_voltage_raw_IR && current_voltage_raw_IR <= IRsensor_VMax_TRESHOLD ){
 			printf_P(PSTR("fsensor v0.4 in fault range 4.6-5V - unconnected\n"));
 			return false;
 		}
-		//! newer IR sensor cannot normally produce 0-0.3V, this is considered a failure 
+		/// newer IR sensor cannot normally produce 0-0.3V, this is considered a failure 
 #if 0	//Disabled as it has to be decided if we gonna use this or not.
 		if( IRsensor_Hopen_TRESHOLD <= current_voltage_raw_IR && current_voltage_raw_IR <= IRsensor_VMax_TRESHOLD ){
 			printf_P(PSTR("fsensor v0.4 in fault range 0.0-0.3V - wrong IR sensor\n"));
@@ -757,7 +757,7 @@ bool fsensor_IR_check(){
 		}
 #endif
 	}
-	//! If IR sensor is "uknown state" and filament is not loaded > 1.5V return false
+	/// If IR sensor is "uknown state" and filament is not loaded > 1.5V return false
 #if 0
 	if( (oFsensorPCB == ClFsensorPCB::_Undef) && ( current_voltage_raw_IR > IRsensor_Lmax_TRESHOLD ) ){
 			printf_P(PSTR("Unknown IR sensor version and no filament loaded detected.\n"));
