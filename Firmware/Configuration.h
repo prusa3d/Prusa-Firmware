5356d9861195bc850c21ffde332256aece5ab673
3d-gussner <3d.gussner@gmail.com>
2020-06-22 10:00:39 +0200

    Bump up FW build nr

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -17,7 +17,7 @@ extern PGM_P sPrinterName;
 
 // Firmware version
 #define FW_VERSION "3.9.1-BETA"
-#define FW_COMMIT_NR 3459
+#define FW_COMMIT_NR 3464
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
 #define FW_DEV_VERSION FW_VERSION_UNKNOWN
