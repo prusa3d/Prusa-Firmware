f34612594848beaed4deef09e6dd19e9ce9d0c04
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2021-02-26 09:18:59 +0200

    Use BubbleSort by default. Disable ShellSort until I fix it

--- Firmware/Configuration_adv.h
+++ Firmware/Configuration_adv.h
@@ -239,7 +239,7 @@
 	  #define SD_SORT_TIME 0
 	  #define SD_SORT_ALPHA 1
 	  #define SD_SORT_NONE 2
-	  #define SHELLSORT
+	  // #define SHELLSORT
 	  // #define SORTING_DUMP
 	
 	  #define SDSORT_LIMIT       100    // Maximum number of sorted items (10-256).
