b3b062e4ba4c3a466149e3571db91601226a2381
3d-gussner <3d.gussner@gmail.com>
2021-03-20 07:05:47 +0100

    Update MSG_SELFTEST_SWAPPED c=16

--- Firmware/ultralcd.cpp
+++ Firmware/ultralcd.cpp
@@ -8113,7 +8113,7 @@ static void lcd_selftest_error(TestError testError, const char *_error_1, const 
 		break;
 	case TestError::SwappedFan:
 		lcd_puts_at_P(0, 2, _i("Front/left fans"));////MSG_SELFTEST_FANS c=20
-		lcd_puts_at_P(0, 3, _i("Swapped"));////MSG_SELFTEST_SWAPPED
+		lcd_puts_at_P(0, 3, _i("Swapped"));////MSG_SELFTEST_SWAPPED c=16
 		lcd_set_cursor(18, 3);
 		lcd_print(_error_1);
 		break;
