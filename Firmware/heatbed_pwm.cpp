7bd43b77d7075dc0d63d832544b1ab08bb5c9ce5
Jonas Meyer <Craftplorer@users.noreply.github.com>
2021-03-27 22:24:48 +0100

    Remove unnecessary assignment

--- Firmware/heatbed_pwm.cpp
+++ Firmware/heatbed_pwm.cpp
@@ -153,7 +153,6 @@ ISR(TIMER0_OVF_vect)          // timer compare interrupt service routine
 			return;           // want full duty for the next ONE cycle again - so keep on heating and just wait for the next timer ovf
 		}
 		// otherwise moving towards FALL
-		state = States::ONE;//_TO_FALL;
 		state=States::FALL;
 		fastCounter = fastMax - 1;// we'll do 16-1 cycles of RISE
 		TCNT0 = 255;              // force overflow on the next clock cycle
