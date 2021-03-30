69b9acdfc1b7e52dc654330ee8a9f383c8614452
3d-gussner <3d.gussner@gmail.com>
2021-03-30 08:57:42 +0200

    Define #MSG_CRASH_RESUME c=20 r=3

--- Firmware/Marlin_main.cpp
+++ Firmware/Marlin_main.cpp
@@ -650,7 +650,7 @@ void crashdet_detected(uint8_t mask)
 		enquecommand_P(PSTR("CRASH_RECOVER"));
 	}else{
 		setTargetHotend(0, active_extruder);
-		bool yesno = lcd_show_fullscreen_message_yes_no_and_wait_P(_i("Crash detected. Resume print?"), false);////c=20 r=2
+		bool yesno = lcd_show_fullscreen_message_yes_no_and_wait_P(_i("Crash detected. Resume print?"), false);////MSG_CRASH_RESUME c=20 r=3
 		lcd_update_enable(true);
 		if (yesno)
 		{
