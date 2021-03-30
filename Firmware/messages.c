ddf47a3cacb2bb4f48cb972319b0f2ee78066155
3d-gussner <3d.gussner@gmail.com>
2021-03-30 17:51:31 +0200

    Update #MSG_NA c=3, #MSG_OFF c=3, #MSG_ON c=3

--- Firmware/messages.c
+++ Firmware/messages.c
@@ -124,9 +124,9 @@ const char MSG_WIZARD_WELCOME_SHIPPING[] PROGMEM_I1 = ISTR("Hi, I am your Origin
 const char MSG_YES[] PROGMEM_I1 = ISTR("Yes"); ////
 const char MSG_V2_CALIBRATION[] PROGMEM_I1 = ISTR("First layer cal."); ////c=18
 const char WELCOME_MSG[] PROGMEM_I1 = ISTR(CUSTOM_MENDEL_NAME " OK."); ////c=20
-const char MSG_OFF[] PROGMEM_I1 = ISTR("Off"); ////
-const char MSG_ON[] PROGMEM_I1 = ISTR("On"); ////
-const char MSG_NA[] PROGMEM_I1 = ISTR("N/A"); ////
+const char MSG_OFF[] PROGMEM_I1 = ISTR("Off"); ////c=3
+const char MSG_ON[] PROGMEM_I1 = ISTR("On"); ////c=3
+const char MSG_NA[] PROGMEM_I1 = ISTR("N/A"); ////c=3
 const char MSG_AUTO_DEPLETE[] PROGMEM_I1 = ISTR("SpoolJoin"); ////
 const char MSG_CUTTER[] PROGMEM_I1 = ISTR("Cutter"); ////c=9
 const char MSG_NONE[] PROGMEM_I1 = ISTR("None"); ////c=8
