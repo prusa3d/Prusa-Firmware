12bd2f1a09f12d43dc72609345ebffd3d01304f5
3d-gussner <3d.gussner@gmail.com>
2021-03-30 09:15:21 +0200

    Update #MSG_PRUSA3D c=18

--- Firmware/ultralcd.cpp
+++ Firmware/ultralcd.cpp
@@ -1916,7 +1916,7 @@ static void lcd_support_menu()
       MENU_ITEM_BACK_P(PSTR("FW - " FW_version));
   }*/
       
-  MENU_ITEM_BACK_P(_i("prusa3d.com"));////MSG_PRUSA3D
+  MENU_ITEM_BACK_P(_i("prusa3d.com"));////MSG_PRUSA3D c=18
   MENU_ITEM_BACK_P(_i("forum.prusa3d.com"));////MSG_PRUSA3D_FORUM c=18
   MENU_ITEM_BACK_P(_i("howto.prusa3d.com"));////MSG_PRUSA3D_HOWTO
   MENU_ITEM_BACK_P(STR_SEPARATOR);
