c1f22a5239376bda3d736b8ccde5fbcd58c3a6f1
3d-gussner <3d.gussner@gmail.com>
2021-03-30 18:12:03 +0200

    Update #MSG_MOVE_AXIS c=18

--- Firmware/ultralcd.cpp
+++ Firmware/ultralcd.cpp
@@ -5592,7 +5592,7 @@ static void lcd_settings_menu()
 
 	if (!PRINTER_ACTIVE || isPrintPaused)
     {
-	    MENU_ITEM_SUBMENU_P(_i("Move axis"), lcd_move_menu_1mm);////MSG_MOVE_AXIS
+	    MENU_ITEM_SUBMENU_P(_i("Move axis"), lcd_move_menu_1mm);////MSG_MOVE_AXIS c=18
 	    MENU_ITEM_GCODE_P(_i("Disable steppers"), PSTR("M84"));////MSG_DISABLE_STEPPERS c=18
     }
 
