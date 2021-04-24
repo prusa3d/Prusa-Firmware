29ad021b230487db8a1e8d7508f966cbab2af3f3
3d-gussner <3d.gussner@gmail.com>
2021-04-24 20:50:01 +0200

    Updated French translation of MSG_MMU_LOAD_FAILED c=20 as @awenelo suggested

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -17,7 +17,7 @@ extern PGM_P sPrinterName;
 
 // Firmware version
 #define FW_VERSION "3.10.0-RC2"
-#define FW_COMMIT_NR 4104
+#define FW_COMMIT_NR 4903
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
 #define FW_DEV_VERSION FW_VERSION_UNKNOWN
