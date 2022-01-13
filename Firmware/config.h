a23fb1dcdac945bc073059db8e6d3109367333b4
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2022-01-13 12:50:12 +0100

    Expand maximum language count

--- Firmware/config.h
+++ Firmware/config.h
@@ -58,7 +58,7 @@
 //#define LANG_MODE              0 // primary language only
 #define LANG_MODE              1 // sec. language support
 
-#define LANG_SIZE_RESERVED     0x3000 // reserved space for secondary language (12288 bytes)
+#define LANG_SIZE_RESERVED     0x3000 // reserved space for secondary language (12288 bytes). Maximum 23768 bytes
 
 //Community language support
 #define COMMUNITY_LANG_GROUP 1
