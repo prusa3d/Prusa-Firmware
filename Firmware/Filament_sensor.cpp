c46b97ec2dfba4845c78bfb98075744e23de7dc7
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2022-02-25 08:20:24 +0100

    Add base code for PAT9125_sensor

--- Firmware/Filament_sensor.cpp
+++ Firmware/Filament_sensor.cpp
@@ -5,5 +5,7 @@
 IR_sensor fsensor;
 #elif FILAMENT_SENSOR_TYPE == FSENSOR_IR_ANALOG
 IR_sensor_analog fsensor;
+#elif FILAMENT_SENSOR_TYPE == FSENSOR_PAT9125
+PAT9125_sensor fsensor;
 #endif
 #endif //FILAMENT_SENSOR
