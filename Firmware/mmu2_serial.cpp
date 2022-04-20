db0374896fe6ae709ef98d71c0ba0e4185cf14fb
D.R.racer <drracer@drracer.eu>
2022-04-20 17:12:00 +0200

    Implement mmu2 serial interface

--- Firmware/mmu2_serial.cpp
+++ Firmware/mmu2_serial.cpp
@@ -1,14 +1,30 @@
 #include "mmu2_serial.h"
-
-//@@TODO implement for MK3
+#include "uart2.h"
 
 namespace MMU2 {
 
-void MMU2Serial::begin(uint32_t baud){ }
-void MMU2Serial::close() { }
-int MMU2Serial::read() { }
-void MMU2Serial::flush() { }
-size_t MMU2Serial::write(const uint8_t *buffer, size_t size) { }
+void MMU2Serial::begin(uint32_t baud){
+    uart2_init(baud); // @@TODO we may skip the baud rate setting in case of 8bit FW ... could save some bytes...
+}
+
+void MMU2Serial::close() {
+    // @@TODO - probably turn off the UART
+}
+
+int MMU2Serial::read() {
+    return fgetc(uart2io);
+}
+
+void MMU2Serial::flush() {
+    // @@TODO - clear the output buffer
+}
+
+size_t MMU2Serial::write(const uint8_t *buffer, size_t size) {
+    while(size--){
+        fputc(*buffer, uart2io);
+        ++buffer;
+    }
+}
 
 MMU2Serial mmu2Serial;
 
