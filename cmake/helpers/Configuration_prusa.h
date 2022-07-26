f03e3c40ee4e6e01384decdd7390985e1e885ae0
VintagePC <53943260+vintagepc@users.noreply.github.com>
2022-07-26 19:33:09 -0400

    WIP/proof-of-concept

---
+++ cmake/helpers/Configuration_prusa.h
@@ -0,0 +1,24 @@
+#ifdef H1_75mm_MK25_RAMBo10a_E3Dv6full
+#include "variants/1_75mm_MK25-RAMBo10a-E3Dv6full.h"
+#endif
+#ifdef H1_75mm_MK25_RAMBo13a_E3Dv6full
+#include "variants/1_75mm_MK25-RAMBo13a-E3Dv6full.h"
+#endif
+#ifdef H1_75mm_MK25S_RAMBo10a_E3Dv6full
+#include "variants/1_75mm_MK25S-RAMBo10a-E3Dv6full.h"
+#endif
+#ifdef H1_75mm_MK25S_RAMBo13a_E3Dv6full
+#include "variants/1_75mm_MK25S-RAMBo13a-E3Dv6full.h"
+#endif
+#ifdef H1_75mm_MK2_RAMBo10a_E3Dv6full
+#include "variants/1_75mm_MK2-RAMBo10a-E3Dv6full.h"
+#endif
+#ifdef H1_75mm_MK2_RAMBo13a_E3Dv6full
+#include "variants/1_75mm_MK2-RAMBo13a-E3Dv6full.h"
+#endif
+#ifdef H1_75mm_MK3_EINSy10a_E3Dv6full
+#include "variants/1_75mm_MK3-EINSy10a-E3Dv6full.h"
+#endif
+#ifdef H1_75mm_MK3S_EINSy10a_E3Dv6full
+#include "variants/1_75mm_MK3S-EINSy10a-E3Dv6full.h"
+#endif
