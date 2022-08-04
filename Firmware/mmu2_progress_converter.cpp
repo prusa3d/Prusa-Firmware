cf67f3d4e5bf61ea8c5ca4ab2f93888c925667b6
D.R.racer <drracer@drracer.eu>
2022-08-04 09:36:38 +0200

    Tune a few MMU progress codes' texts to fit on the LCD

--- Firmware/mmu2_progress_converter.cpp
+++ Firmware/mmu2_progress_converter.cpp
@@ -4,34 +4,34 @@
 #include <avr/pgmspace.h>
 
 namespace MMU2 {
-
-static const char progressOk[] PROGMEM_I1 = ISTR("OK");
-static const char progressEngageIdler[] PROGMEM_I1 = ISTR("Engaging idler");
-static const char progressDisengeIdler[] PROGMEM_I1 = ISTR("Disengaging idler");
-static const char progressUnloadFinda[] PROGMEM_I1 = ISTR("Unloading to FINDA");
-static const char progressUnloadPulley[] PROGMEM_I1 = ISTR("Unloading to pulley");
-static const char progressFeedFinda[] PROGMEM_I1 = ISTR("Feeding to FINDA");
-static const char progressFeedBondtech[] PROGMEM_I1 = ISTR("Feeding to drive gear");
-static const char progressFeedNozzle[] PROGMEM_I1 = ISTR("Feeding to nozzle");
-static const char progressAvoidGrind[] PROGMEM_I1 = ISTR("Avoiding grind");
-static const char progressFinishMoves[] PROGMEM_I1 = ISTR("Finishing moves");
-static const char progressWaitForUser[] PROGMEM_I1 = ISTR("ERR Wait for User");
-static const char progressErrInternal[] PROGMEM_I1 = ISTR("ERR Internal");
-static const char progressErrHelpFil[] PROGMEM_I1 = ISTR("ERR Helping filament");
-static const char progressErrTmc[] PROGMEM_I1 = ISTR("ERR TMC failed");
-static const char progressUnloadFilament[] PROGMEM_I1 = ISTR("Unloading filament");
-static const char progressLoadFilament[] PROGMEM_I1 = ISTR("Loading filament");
-static const char progressSelectSlot[] PROGMEM_I1 = ISTR("Selecting filament slot");
-static const char progressPrepareBlade[] PROGMEM_I1 = ISTR("Preparing blade");
-static const char progressPushFilament[] PROGMEM_I1 = ISTR("Pushing filament");
-static const char progressPerformCut[] PROGMEM_I1 = ISTR("Performing cut");
-static const char progressReturnSelector[] PROGMEM_I1 = ISTR("Returning selector");
-static const char progressParkSelector[] PROGMEM_I1 = ISTR("Parking selector");
-static const char progressEjectFilament[] PROGMEM_I1 = ISTR("Ejecting filament");
-static const char progressRetractFinda[] PROGMEM_I1 = ISTR("Retracting from FINDA");
-static const char progressHoming[] PROGMEM_I1 = ISTR("Homing");
-static const char progressMovingSelector[] PROGMEM_I1 = ISTR("Moving selector");
-static const char progressFeedingToFSensor[] PROGMEM_I1 = ISTR("FeedingToFSensor");
+                                                             // 12345678901234567890
+static const char progressOk[] PROGMEM_I1               = ISTR("OK");
+static const char progressEngageIdler[] PROGMEM_I1      = ISTR("Engaging idler");
+static const char progressDisengeIdler[] PROGMEM_I1     = ISTR("Disengaging idler");
+static const char progressUnloadFinda[] PROGMEM_I1      = ISTR("Unloading to FINDA");
+static const char progressUnloadPulley[] PROGMEM_I1     = ISTR("Unloading to pulley");
+static const char progressFeedFinda[] PROGMEM_I1        = ISTR("Feeding to FINDA");
+static const char progressFeedBondtech[] PROGMEM_I1     = ISTR("Feeding to extruder");
+static const char progressFeedNozzle[] PROGMEM_I1       = ISTR("Feeding to nozzle");
+static const char progressAvoidGrind[] PROGMEM_I1       = ISTR("Avoiding grind");
+static const char progressFinishMoves[] PROGMEM_I1      = ISTR("Finishing moves");
+static const char progressWaitForUser[] PROGMEM_I1      = ISTR("ERR Wait for User");
+static const char progressErrInternal[] PROGMEM_I1      = ISTR("ERR Internal");
+static const char progressErrHelpFil[] PROGMEM_I1       = ISTR("ERR Help filament");
+static const char progressErrTmc[] PROGMEM_I1           = ISTR("ERR TMC failed");
+static const char progressUnloadFilament[] PROGMEM_I1   = ISTR("Unloading filament");
+static const char progressLoadFilament[] PROGMEM_I1     = ISTR("Loading filament");
+static const char progressSelectSlot[] PROGMEM_I1       = ISTR("Selecting fil. slot");
+static const char progressPrepareBlade[] PROGMEM_I1     = ISTR("Preparing blade");
+static const char progressPushFilament[] PROGMEM_I1     = ISTR("Pushing filament");
+static const char progressPerformCut[] PROGMEM_I1       = ISTR("Performing cut");
+static const char progressReturnSelector[] PROGMEM_I1   = ISTR("Returning selector");
+static const char progressParkSelector[] PROGMEM_I1     = ISTR("Parking selector");
+static const char progressEjectFilament[] PROGMEM_I1    = ISTR("Ejecting filament");
+static const char progressRetractFinda[] PROGMEM_I1     = ISTR("Retract from FINDA");
+static const char progressHoming[] PROGMEM_I1           = ISTR("Homing");
+static const char progressMovingSelector[] PROGMEM_I1   = ISTR("Moving selector");
+static const char progressFeedingToFSensor[] PROGMEM_I1 = ISTR("Feeding to FSensor");
 
 static const char * const progressTexts[] PROGMEM = {
     progressOk,
