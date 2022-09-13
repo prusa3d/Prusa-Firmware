1e3314f14578f36d0001d0cb02776667eea32354
3d-gussner <3d.gussner@gmail.com>
2022-09-13 14:53:12 +0200

    Set reserved space back after cherry-pick

--- Firmware/config.h
+++ Firmware/config.h
@@ -59,7 +59,7 @@
 //#define LANG_MODE              0 // primary language only
 #define LANG_MODE              1 // sec. language support
 
-#define LANG_SIZE_RESERVED     0x3900 // reserved space for secondary language (14592 bytes).
+#define LANG_SIZE_RESERVED     0x3000 // reserved space for secondary language (12288 bytes).
                                       // 0x3D00 Maximum 15616 bytes as it depends on xflash_layout.h
                                       // 16 Languages max. per group including stock 
 
