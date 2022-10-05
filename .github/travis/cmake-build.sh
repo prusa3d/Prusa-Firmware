f39feae15fc6fac700a729dbdd2e20b140836a57
3d-gussner <3d.gussner@gmail.com>
2022-10-05 18:51:33 +0200

    No need for PATH

--- .github/travis/cmake-build.sh
+++ .github/travis/cmake-build.sh
@@ -6,9 +6,6 @@ dep_path() { ./utils/bootstrap.py --print-dependency-directory "$1"; }
 CMAKE_PATH=$(dep_path cmake)/bin
 NINJA_PATH=$(dep_path ninja)
 
-PATH=$PWD/$CMAKE_PATH:$PATH
-PATH=$PWD/$NINJA_PATH:$PATH
-
 rm -rf build
 
 $CMAKE_PATH/cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_TOOLCHAIN_FILE:FILEPATH=$PWD/cmake/LocalAvrGcc.cmake -DCMAKE_MAKE_PROGRAM:STRING=$NINJA_PATH/ninja -S$PWD -B$PWD/build -G Ninja
