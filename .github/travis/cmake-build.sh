cf5248d4af53271cc45344dfdf1c5545e118d0b1
3d-gussner <3d.gussner@gmail.com>
2022-10-05 18:55:04 +0200

    Check for python

--- .github/travis/cmake-build.sh
+++ .github/travis/cmake-build.sh
@@ -6,6 +6,9 @@ dep_path() { ./utils/bootstrap.py --print-dependency-directory "$1"; }
 CMAKE_PATH=$(dep_path cmake)/bin
 NINJA_PATH=$(dep_path ninja)
 
+python --version
+python3 --version
+
 rm -rf build
 
 $CMAKE_PATH/cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_TOOLCHAIN_FILE:FILEPATH=$PWD/cmake/LocalAvrGcc.cmake -DCMAKE_MAKE_PROGRAM:STRING=$NINJA_PATH/ninja -S$PWD -B$PWD/build -G Ninja
