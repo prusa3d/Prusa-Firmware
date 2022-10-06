80594880cd68dd78dde0a5f7d176859bbc93504f
Yuri D'Elia <wavexx@thregr.org>
2022-10-06 12:48:07 +0200

    cmake: Simplify version handling based on git tags
    - Use the HEAD commit hash as the default suffix
    - Use the output of "describe" as the full version string
    - Use the HEAD commit time as SOURCE_DATE_EPOCH

--- cmake/GetGitRevisionDescription.cmake
+++ cmake/GetGitRevisionDescription.cmake
@@ -334,3 +334,27 @@ function(git_head_commit_timestamp _var)
       PARENT_SCOPE
       )
 endfunction()
+
+function(git_head_commit_data _var _format)
+    if(NOT GIT_FOUND)
+        find_package(Git QUIET)
+    endif()
+    if(NOT GIT_FOUND)
+        set(${_var}
+            "GIT-NOTFOUND"
+            PARENT_SCOPE)
+        return()
+    endif()
+    execute_process(
+        COMMAND "${GIT_EXECUTABLE}" show -s --quiet --format=format:${_format} HEAD
+        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+        RESULT_VARIABLE res
+        OUTPUT_VARIABLE out
+        ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(NOT res EQUAL 0)
+        set(out "HEAD-FORMAT-NOTFOUND")
+    endif()
+    set(${_var}
+        ${out}
+        PARENT_SCOPE)
+endfunction()
