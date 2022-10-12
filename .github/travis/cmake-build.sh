788b89a9974dd466391cca7b0ab94919e40651bc
Yuri D'Elia <wavexx@thregr.org>
2022-10-12 11:13:12 +0200

    build: Switch travis to AvrGcc

--- .github/travis/cmake-build.sh
+++ .github/travis/cmake-build.sh
@@ -4,7 +4,7 @@ rm -rf build
 mkdir build
 cd build
 cmake .. \
-    -DCMAKE_TOOLCHAIN_FILE="../cmake/LocalAvrGcc.cmake" \
+    -DCMAKE_TOOLCHAIN_FILE="../cmake/AvrGcc.cmake" \
     -DCMAKE_BUILD_TYPE=Release \
     -G Ninja
 ninja ALL_FIRMWARE
