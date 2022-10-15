2e7c237a17bc54d21df171b44195ecf1e2c97e8c
Guðni Már Gilbert <gudni.m.g@gmail.com>
2022-10-15 08:08:30 +0000

    Cleanup: remove automatic parameter from unload_filament()
    In M600 and M702, the Z-lift is taken care of outside this function
    so it makes no sense to have a Z-lift within unload_filament()
    
    Only exception to this is in the wizard, there I moved the Z-lift to
    happen before we start heating the nozzle.
    
    Change in memory:
    Flash: -12 bytes
    SRAM: 0 bytes

--- Firmware/ultralcd.h
+++ Firmware/ultralcd.h
@@ -197,7 +197,7 @@ extern bool bFilamentAction;
 void mFilamentItem(uint16_t nTemp,uint16_t nTempBed);
 void mFilamentItemForce();
 void lcd_generic_preheat_menu();
-void unload_filament(float unloadLength, bool automatic = false);
+void unload_filament(float unloadLength);
 
 
 void lcd_wait_for_heater();
