f4918622f0b08f66bb2d56957155b523adc0e0e8
Yuri D'Elia <wavexx@thregr.org>
2022-10-22 16:32:56 +0200

    build: Update tests for Catch2 v3

--- tests/Example_test.cpp
+++ tests/Example_test.cpp
@@ -1,4 +1,4 @@
-#include "catch2/catch.hpp"
+#include "catch2/catch_test_macros.hpp"
 
 unsigned int Factorial( unsigned int number ) {
     return number <= 1 ? number : Factorial(number-1)*number;
