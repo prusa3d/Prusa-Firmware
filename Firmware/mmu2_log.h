1e0909d50c06c015bbefcb1f8816f41c4056bc49
3d-gussner <3d.gussner@gmail.com>
2022-10-27 12:36:30 +0200

    Disable `Error:` messages for MMU2 errors

--- Firmware/mmu2_log.h
+++ Firmware/mmu2_log.h
@@ -23,9 +23,9 @@ void LogEchoEvent_P(const char *msg);
 #define SERIAL_MMU2() { serialprintPGM(mmu2Magic); }
 
 #define MMU2_ECHO_MSG(S) do{ SERIAL_ECHO_START; SERIAL_MMU2(); SERIAL_ECHO(S); }while(0)
-#define MMU2_ERROR_MSG(S) do{ SERIAL_ERROR_START; SERIAL_MMU2(); SERIAL_ECHO(S); }while(0)
+#define MMU2_ERROR_MSG(S) MMU2_ECHO_MSG(S) //!@todo Decide MMU2 errors  on serial line
 #define MMU2_ECHO_MSGRPGM(S) do{ SERIAL_ECHO_START; SERIAL_MMU2(); SERIAL_ECHORPGM(S); }while(0)
-#define MMU2_ERROR_MSGRPGM(S) do{ SERIAL_ERROR_START; SERIAL_MMU2(); SERIAL_ECHORPGM(S); }while(0)
+#define MMU2_ERROR_MSGRPGM(S) MMU2_ECHO_MSGRPGM(S) //!@todo Decide MMU2 errors  on serial line
 
 #else // #ifndef UNITTEST
 
