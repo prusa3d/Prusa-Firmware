1dcdedb90ef1cbaec456570f3e73ba433eb21833
Yuri D'Elia <wavexx@thregr.org>
2022-12-21 13:08:26 +0100

    Typo: hysterezis -> hysteresis

--- Firmware/xyzcal.h
+++ Firmware/xyzcal.h
@@ -15,6 +15,6 @@ extern bool xyzcal_spiral2(int16_t cx, int16_t cy, int16_t z0, int16_t dz, int16
 
 extern bool xyzcal_spiral8(int16_t cx, int16_t cy, int16_t z0, int16_t dz, int16_t radius, uint16_t delay_us, int8_t check_pinda, uint16_t* pad);
 
-//extern int8_t xyzcal_meassure_pinda_hysterezis(int16_t min_z, int16_t max_z, uint16_t delay_us, uint8_t samples);
+//extern int8_t xyzcal_measure_pinda_hysteresis(int16_t min_z, int16_t max_z, uint16_t delay_us, uint8_t samples);
 
 extern BedSkewOffsetDetectionResultType xyzcal_find_bed_induction_sensor_point_xy();
