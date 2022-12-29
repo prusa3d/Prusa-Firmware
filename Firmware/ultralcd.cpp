d8ae372309102b5288ca5133e0ce2e1b4e2b488b
Yuri D'Elia <wavexx@thregr.org>
2022-12-29 13:21:52 +0100

    wizard: Allow partial resuming after reset
    Update the eeprom FW version as soon as migration is complete, to avoid
    resetting the CALIBRATION_V2 variable at each reset.
    
    Do not implicitly reset the calibration steps for WizState::Run: do this
    only for the menu action.

--- Firmware/ultralcd.cpp
+++ Firmware/ultralcd.cpp
@@ -3974,6 +3974,7 @@ void lcd_wizard() {
 		result = !lcd_show_multiscreen_message_yes_no_and_wait_P(_i("Running Wizard will delete current calibration results and start from the beginning. Continue?"), false);////MSG_WIZARD_RERUN c=20 r=7
 	}
 	if (result) {
+		calibration_status_clear(CALIBRATION_WIZARD_STEPS);
 		lcd_wizard(WizState::Run);
 	} else {
 		lcd_return_to_status();
@@ -4135,7 +4136,6 @@ void lcd_wizard(WizState state)
 				if (wizard_event == LCD_LEFT_BUTTON_CHOICE) {
 					state = S::Restore;
 					eeprom_update_byte((uint8_t*)EEPROM_WIZARD_ACTIVE, 1);
-					calibration_status_clear(CALIBRATION_WIZARD_STEPS);
 				} else {
 					// user interrupted
 					eeprom_update_byte((uint8_t*)EEPROM_WIZARD_ACTIVE, 0);
