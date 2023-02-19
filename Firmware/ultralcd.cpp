319fec47f0e32208f949bf26b61e2099e020846a
Guðni Már Gilbert <gudni.m.g@gmail.com>
2023-02-19 12:43:21 +0000

    preheat: improve target temperature threshold
    Use 5°C threshold to be consistant with other parts
    of the firmware.
    
    Relying on 95% of the target temperature creates
    a dependency on the temperature:
    
    PLA: Target = 215°C, threshold = 10.75°C
    PETG: Target = 230°C, threshold = 11.5°C
    ABS: Target = 255°C, threshold  = 12.75°C
    ASA: Target =260°C, threshold = 13.0°C
    PC: Target = 275°C, threshold  = 13.75°C
    
    My proposal is we instead use a constant
    TEMP_HYSTERESIS = 5, which is consistent with
    M109, and behavior when restoring print from RAM
    and some of the MMU code (like unload function)
    
    Change in memory:
    Flash: +2 bytes
    SRAM: 0 bytes

--- Firmware/ultralcd.cpp
+++ Firmware/ultralcd.cpp
@@ -1888,7 +1888,7 @@ void mFilamentItem(uint16_t nTemp, uint16_t nTempBed)
 
     lcd_timeoutToStatus.stop();
 
-    if (current_temperature[0] > (target_temperature[0] * 0.95))
+    if (fabs(current_temperature[0] - target_temperature[0]) > TEMP_HYSTERESIS)
     {
         switch (eFilamentAction)
         {
