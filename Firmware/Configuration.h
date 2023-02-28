662f5e0248fae462f1e107f9c1ed2a904d0694a0
3d-gussner <3d.gussner@gmail.com>
2023-02-28 08:08:59 +0100

    Bump up FW 3.12.2 to 5713

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -27,7 +27,7 @@ extern PGM_P sPrinterName;
     #define FW_VERSION STR(FW_MAJOR) "." STR(FW_MINOR) "." STR(FW_REVISION) "-" STR(FW_FLAVOR) "" STR(FW_FLAVERSION)
 #endif
 
-#define FW_COMMIT_NR 5697
+#define FW_COMMIT_NR 5713
 
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
