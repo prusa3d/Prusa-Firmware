b837c11840b6e9160ab81487e3848b0734138aeb
D.R.racer <drracer@drracer.eu>
2023-03-14 09:43:40 +0100

    Disable reading MMU register 0x21
    It has proven to be too unreliable and may confuse end users.

--- Firmware/mmu2_protocol_logic.cpp
+++ Firmware/mmu2_protocol_logic.cpp
@@ -35,7 +35,6 @@ const uint8_t ProtocolLogic::regs8Addrs[ProtocolLogic::regs8Count] PROGMEM = {
 const uint8_t ProtocolLogic::regs16Addrs[ProtocolLogic::regs16Count] PROGMEM = {
     4,    // MMU errors - aka statistics
     0x1a, // Pulley position [mm]
-    0x21, // MCU bandgap voltage
 };
 
 const uint8_t ProtocolLogic::initRegs8Addrs[ProtocolLogic::initRegs8Count] PROGMEM = {
