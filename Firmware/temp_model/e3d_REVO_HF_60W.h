a2308aea8b884225d10fb938eb4c26bcb514e3d9
3d-gussner <3d.gussner@gmail.com>
2023-06-15 15:26:12 +0200

    Update E3DREVO High Flow 60W

---
+++ Firmware/temp_model/e3d_REVO_HF_60W.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define TEMP_MODEL_E3D_REVO_HF_60W_VER 1      // model parameters version
+
+#define TEMP_MODEL_E3D_REVO_HF_60W_P 60.      // heater power (W)
+#define TEMP_MODEL_E3D_REVO_HF_60W_U -0.0014  // linear temperature coefficient (W/K/power)
+#define TEMP_MODEL_E3D_REVO_HF_60W_V 1.05     // linear temperature intercept (W/power)
+
+#define TEMP_MODEL_E3D_REVO_HF_60W_C 9.10     // initial guess for heatblock capacitance (J/K)
+#define TEMP_MODEL_E3D_REVO_HF_60W_R 30.6     // initial guess for heatblock resistance (K/W)
+
+#define TEMP_MODEL_E3D_REVO_HF_60W_fS 0.15    // sim. 1st order IIR filter factor (f=100/27)
+#define TEMP_MODEL_E3D_REVO_HF_60W_LAG 270    // sim. response lag (ms, 0-2160)
+
+#define TEMP_MODEL_E3D_REVO_HF_60W_W 0.75     // Default warning threshold (K/s)
+#define TEMP_MODEL_E3D_REVO_HF_60W_E 0.90     // Default error threshold (K/s)
+
+// fall-back resistance vector (R0-15)
+#define TEMP_MODEL_E3D_REVO_HF_60W_Rv {TEMP_MODEL_E3D_REVO_HF_60W_R, 29.0, 27.5, 24.5, 23.4, 22.3, 21.2, 20.2, 19.8, 19.4, 19.0, 18.6, 18.3, 18.1, 17.9, 17.7}
