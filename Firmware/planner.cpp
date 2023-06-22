c4fc1a86151150ead6788f3917d43d64aa131469
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2023-06-22 13:41:02 +0200

    Optimize `st_set_position()` for speed and size
    flash: -186
    SRAM: 0
    time: x0.45

--- Firmware/planner.cpp
+++ Firmware/planner.cpp
@@ -1330,7 +1330,7 @@ void plan_set_position(float x, float y, float z, const float &e)
   position_float[Z_AXIS] = z;
   position_float[E_AXIS] = e;
   #endif
-  st_set_position(position[X_AXIS], position[Y_AXIS], position[Z_AXIS], position[E_AXIS]);
+  st_set_position(position);
   previous_nominal_speed = 0.0; // Resets planner junction speeds. Assumes start from rest.
   memset(previous_speed, 0, sizeof(previous_speed));
 }
@@ -1342,7 +1342,7 @@ void plan_set_z_position(const float &z)
   position_float[Z_AXIS] = z;
   #endif
   position[Z_AXIS] = lround(z*cs.axis_steps_per_unit[Z_AXIS]);
-  st_set_position(position[X_AXIS], position[Y_AXIS], position[Z_AXIS], position[E_AXIS]);
+  st_set_position(position);
 }
 
 void plan_set_e_position(const float &e)
