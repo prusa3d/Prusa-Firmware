189317db492e6bbeaf2111a5b5440dfe37425572
Guðni Már Gilbert <gudni.m.g@gmail.com>
2023-06-23 21:33:44 +0000

    Remove Marlin.h include in backlight.h
    Also removed it from sound.cpp, only include what is really required

--- Firmware/backlight.cpp
+++ Firmware/backlight.cpp
@@ -1,12 +1,12 @@
 //backlight.cpp
-
-#include "backlight.h"
-#include "macros.h"
-#include <avr/eeprom.h>
 #include <Arduino.h>
+#include <avr/eeprom.h>
+#include "backlight.h"
 #include "eeprom.h"
-#include "pins.h"
 #include "fastio.h"
+#include "macros.h"
+#include "pins.h"
+#include "system_timer.h"
 #include "Timer.h"
 
 #ifdef LCD_BL_PIN
