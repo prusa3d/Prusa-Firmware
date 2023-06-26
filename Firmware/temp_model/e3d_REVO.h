5f9cece2da65fefc02c03565994ed788b3e1c46d
3d-gussner <3d.gussner@gmail.com>
2023-06-26 10:32:13 +0200

    Update REVO TM warning a error values

--- Firmware/temp_model/e3d_REVO.h
+++ Firmware/temp_model/e3d_REVO.h
@@ -12,8 +12,8 @@
 #define TEMP_MODEL_E3D_REVO_fS 0.15    // sim. 1st order IIR filter factor (f=100/27)
 #define TEMP_MODEL_E3D_REVO_LAG 270    // sim. response lag (ms, 0-2160)
 
-#define TEMP_MODEL_E3D_REVO_W 1.2      // Default warning threshold (K/s)
-#define TEMP_MODEL_E3D_REVO_E 1.74     // Default error threshold (K/s)
+#define TEMP_MODEL_E3D_REVO_W 0.85     // Default warning threshold (K/s)
+#define TEMP_MODEL_E3D_REVO_E 1.23     // Default error threshold (K/s)
 
 // fall-back resistance vector (R0-15)
 #define TEMP_MODEL_E3D_REVO_Rv {TEMP_MODEL_E3D_REVO_R, 23.9, 22.5, 19.6, 19.0, 18.3, 17.7, 17.1, 16.8, 16.5, 16.3, 16.0, 15.9, 15.7, 15.6, 15.4}
--- Firmware/temp_model/e3d_REVO_HF_60W.h
+++ Firmware/temp_model/e3d_REVO_HF_60W.h
@@ -12,8 +12,8 @@
 #define TEMP_MODEL_E3D_REVO_HF_60W_fS 0.15    // sim. 1st order IIR filter factor (f=100/27)
 #define TEMP_MODEL_E3D_REVO_HF_60W_LAG 270    // sim. response lag (ms, 0-2160)
 
-#define TEMP_MODEL_E3D_REVO_HF_60W_W 0.75     // Default warning threshold (K/s)
-#define TEMP_MODEL_E3D_REVO_HF_60W_E 0.90     // Default error threshold (K/s)
+#define TEMP_MODEL_E3D_REVO_HF_60W_W 0.85     // Default warning threshold (K/s)
+#define TEMP_MODEL_E3D_REVO_HF_60W_E 1.23     // Default error threshold (K/s)
 
 // fall-back resistance vector (R0-15)
 #define TEMP_MODEL_E3D_REVO_HF_60W_Rv {TEMP_MODEL_E3D_REVO_HF_60W_R, 29.0, 27.5, 24.5, 23.4, 22.3, 21.2, 20.2, 19.8, 19.4, 19.0, 18.6, 18.3, 18.1, 17.9, 17.7}
