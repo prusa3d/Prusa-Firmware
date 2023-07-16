84900426a71422d54801b413f933496461eb1083
Guðni Már Gilbert <gudni.m.g@gmail.com>
2023-07-16 14:43:08 +0000

    power panic: add readable flags and optimise ISR(INT4_vect)
    Add more readable names for the EEPROM_UVLO values
    
    Change in memory:
    Flash: -14 bytes
    SRAM: 0 bytes

--- Firmware/power_panic.h
+++ Firmware/power_panic.h
@@ -1,5 +1,11 @@
 #pragma once
 
+enum PowerPanicFlag : uint8_t {
+    NO_PENDING_RECOVERY = 0,
+    PENDING_RECOVERY = 1, // First power panic, print state is saved in EEPROM
+    PENDING_RECOVERY_RETRY = 2, // Power outage occured during recovery, print is still saved in EEPROM
+};
+
 void uvlo_();
 void recover_print(uint8_t automatic); 
 void setup_uvlo_interrupt();
