prusa3d/Prusa-Firmware

commit 136ef9696d51715481a6e3f3774d9eaef3ea5d57

Do not check MMU version with `M862.3` for MK3|MK3S

Firmware/util.cpp
@@ -408,14 +408,14 @@ return pStrBegin;
 
 void printer_smodel_check(const char *pStrPos, const char *actualPrinterSModel) {
 char* pResult;
-size_t nLength,nPrinterNameLength;
+size_t nLength;
 
-nPrinterNameLength = strlen_P(actualPrinterSModel);
 pResult=code_string(pStrPos,&nLength);
 
-if(pResult != NULL && nLength == nPrinterNameLength) {
-     // Only compare them if the lengths match
-     if (strncmp_P(pResult, actualPrinterSModel, nLength) == 0) return;
+if(pResult != NULL) {
+    // Only compare first 6 chars on MK3|MK3S
+    if (strncmp_P(pResult, PSTR("MK3"), 3) == 0) nLength = 6;
+    if (strncmp_P(pResult, actualPrinterSModel, nLength) == 0) return;
 }
 
     render_M862_warnings(

commit 2bb6ea58596c997bd89ec685f6f21e8ea33a12fd

Fixed printer_smodel_check for non MMU machines
https://github.com/prusa3d/Prusa-Firmware/issues/4258

Commit 136ef96 broke the compatibility check for MK3S without MMU.

Firmware/util.cpp
@@ -407,16 +407,21 @@ return pStrBegin;
 }
 
 void printer_smodel_check(const char *pStrPos, const char *actualPrinterSModel) {
-char* pResult;
-size_t nLength;
+    char* pResult;
+    char gStr[12];
+    size_t nLength;
 
-pResult=code_string(pStrPos,&nLength);
+    pResult=code_string(pStrPos,&nLength);
+    if(pResult != NULL) {
 
-if(pResult != NULL) {
-    // Only compare first 6 chars on MK3|MK3S
-    if (strncmp_P(pResult, PSTR("MK3"), 3) == 0) nLength = 6;
-    if (strncmp_P(pResult, actualPrinterSModel, nLength) == 0) return;
-}
+        if(nLength > 11) nLength = 11;
+        memcpy(gStr, pResult, nLength);
+        gStr[nLength] = 0;
+
+        // Only compare first 5 chars on MK3|MK3S
+        if(strncmp_P(gStr, PSTR("MK3"), 3) == 0) nLength = 5;    
+        if (strncmp_P(gStr, actualPrinterSModel, nLength) == 0) return;
+    }
 
     render_M862_warnings(
         _T(MSG_GCODE_DIFF_PRINTER_CONTINUE)