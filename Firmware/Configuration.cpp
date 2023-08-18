6c0f80e6809ffb403cc1e02f867d94e22ccd5c8c
sarusani <62053849+sarusani@users.noreply.github.com>
2023-08-18 16:01:20 +0200

    Add static_assert check for TEMP_HYSTERESIS

--- Firmware/Configuration.cpp
+++ Firmware/Configuration.cpp
@@ -5,3 +5,5 @@ const uint16_t _nPrinterType PROGMEM=PRINTER_TYPE;
 const char _sPrinterName[] PROGMEM=PRINTER_NAME;
 const uint16_t _nPrinterMmuType PROGMEM=PRINTER_MMU_TYPE;
 const char _sPrinterMmuName[] PROGMEM=PRINTER_MMU_NAME;
+
+static_assert(TEMP_HYSTERESIS > 0, "TEMP_HYSTERESIS must be greater than 0");
