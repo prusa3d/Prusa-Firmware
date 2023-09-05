2fd51769b396c8a3517795aea5793ea1d486951b
3d-gussner <3d.gussner@gmail.com>
2023-09-05 15:26:33 +0200

    Update pot and po files Bump up to FW 3.13.2-RC1 and build number +2 (+1 commit + 1 merge)

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -15,7 +15,7 @@ extern const char _sPrinterMmuName[] PROGMEM;
 // Firmware version
 #define FW_MAJOR 3
 #define FW_MINOR 13
-#define FW_REVISION 1
+#define FW_REVISION 2
 #define FW_FLAVOR RC      //uncomment if DEBUG, DEVEL, ALPHA, BETA or RC
 #define FW_FLAVERSION 1     //uncomment if FW_FLAVOR is defined and versioning is needed. Limited to max 8.
 #ifndef FW_FLAVOR
@@ -24,7 +24,7 @@ extern const char _sPrinterMmuName[] PROGMEM;
     #define FW_VERSION STR(FW_MAJOR) "." STR(FW_MINOR) "." STR(FW_REVISION) "-" STR(FW_FLAVOR) "" STR(FW_FLAVERSION)
 #endif
 
-#define FW_COMMIT_NR 7052
+#define FW_COMMIT_NR 7068
 
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
