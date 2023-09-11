4684718abcdf3bb61ec74012e3ccafa4bbee2536
D.R.racer <drracer@drracer.eu>
2023-09-11 09:17:29 +0200

    Sync MK3<->MK4 MMU2 crc

--- Firmware/mmu2_crc.cpp
+++ Firmware/mmu2_crc.cpp
@@ -2,12 +2,12 @@
 #include "mmu2_crc.h"
 
 #ifdef __AVR__
-#include <util/crc16.h>
+    #include <util/crc16.h>
 #endif
 
 namespace modules {
+// clang-format off
 namespace crc {
-
 #ifdef __AVR__
 uint8_t CRC8::CCITT_update(uint8_t crc, uint8_t b) {
     return _crc8_ccitt_update(crc, b);
@@ -17,6 +17,6 @@ uint8_t CRC8::CCITT_update(uint8_t crc, uint8_t b) {
     return CCITT_updateCX(crc, b);
 }
 #endif
-
 } // namespace crc
+// clang-format on
 } // namespace modules
