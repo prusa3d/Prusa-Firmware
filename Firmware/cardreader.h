18132201a7f9a25b8fd689e07158560b91fd616f
Juan Francisco Estrada <juanfra@cloudhd.eu>
2023-11-06 15:36:02 +0100

    Apply the reprint_pr_changes_diff2.txt provided by @3d-gussner

--- Firmware/cardreader.h
+++ Firmware/cardreader.h
@@ -83,7 +83,7 @@ public:
   bool ToshibaFlashAir_isEnabled() const { return card.getFlashAirCompatible(); }
   void ToshibaFlashAir_enable(bool enable) { card.setFlashAirCompatible(enable); }
   bool ToshibaFlashAir_GetIP(uint8_t *ip);
-  
+
   //Reprint
   bool FileExists(const char* filename);
 
