a6941397c5def7f5dd92ba1f3d58661792c43923
Juan Francisco Estrada <juanfra@cloudhd.eu>
2023-11-22 22:44:31 +0100

    Revert "mmu: formatting"
    This reverts commit 53c9d65e03374d9407e0deae7a277aee1d1155af.

--- Firmware/mmu2.cpp
+++ Firmware/mmu2.cpp
@@ -835,58 +835,45 @@ bool MMU2::manage_response(const bool move_axes, const bool turn_off_nozzle) {
 }
 
 StepStatus MMU2::LogicStep(bool reportErrors) {
-    // Process any buttons before proceeding with another MMU Query
-    CheckUserInput();
-
-    const StepStatus ss = logic.Step();
+    CheckUserInput(); // Process any buttons before proceeding with another MMU Query
+    StepStatus ss = logic.Step();
     switch (ss) {
-
     case Finished:
         // At this point it is safe to trigger a runout and not interrupt the MMU protocol
         CheckFINDARunout();
         break;
-
     case Processing:
         OnMMUProgressMsg(logic.Progress());
         break;
-
     case ButtonPushed:
         lastButton = logic.Button();
         LogEchoEvent_P(PSTR("MMU Button pushed"));
         CheckUserInput(); // Process the button immediately
         break;
-
     case Interrupted:
         // can be silently handed over to a higher layer, no processing necessary at this spot
         break;
-
     default:
         if (reportErrors) {
             switch (ss) {
-
             case CommandError:
                 ReportError(logic.Error(), ErrorSourceMMU);
                 break;
-
             case CommunicationTimeout:
                 state = xState::Connecting;
                 ReportError(ErrorCode::MMU_NOT_RESPONDING, ErrorSourcePrinter);
                 break;
-
             case ProtocolError:
                 state = xState::Connecting;
                 ReportError(ErrorCode::PROTOCOL_ERROR, ErrorSourcePrinter);
                 break;
-
             case VersionMismatch:
                 StopKeepPowered();
                 ReportError(ErrorCode::VERSION_MISMATCH, ErrorSourcePrinter);
                 break;
-
             case PrinterError:
                 ReportError(logic.PrinterError(), ErrorSourcePrinter);
                 break;
-
             default:
                 break;
             }
@@ -896,7 +883,6 @@ StepStatus MMU2::LogicStep(bool reportErrors) {
     if (logic.Running()) {
         state = xState::Active;
     }
-
     return ss;
 }
 
