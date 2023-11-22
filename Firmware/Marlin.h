6afa3d6ccb1b11234add995656b2a1fbfb2ac9be
Juan Francisco Estrada <juanfra@cloudhd.eu>
2023-11-22 22:40:35 +0100

    Revert "Add M125"
    This reverts commit 1b9704fab98650840ba636cef7fb005199c0cd2c.

--- Firmware/Marlin.h
+++ Firmware/Marlin.h
@@ -232,7 +232,6 @@ extern int feedmultiply;
 extern int extrudemultiply; // Sets extrude multiply factor (in percent) for all extruders
 extern float extruder_multiplier[EXTRUDERS]; // reciprocal of cross-sectional area of filament (in square millimeters), stored this way to reduce computational burden in planner
 extern float current_position[NUM_AXIS] ;
-extern float pause_position[3];
 extern float destination[NUM_AXIS] ;
 extern float min_pos[3];
 extern float max_pos[3];
