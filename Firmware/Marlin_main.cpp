f2a75b495a25a61f08be525ba4e9c5f3affcda38
gudnimg <gudni.m.g@gmail.com>
2023-11-25 14:41:57 +0000

    power panic: remove redundant lcd update calls
    lcd_show_fullscreen_message_yes_no_and_wait_P will
    enable LCD update upon exit.
    
    Change in memory:
    Flash: -24 bytes
    SRAM: 0 bytes

--- Firmware/Marlin_main.cpp
+++ Firmware/Marlin_main.cpp
@@ -1595,12 +1595,11 @@ void setup()
           #ifdef DEBUG_UVLO_AUTOMATIC_RECOVER 
         puts_P(_N("Normal recovery!")); 
           #endif
-          if ( lcd_show_fullscreen_message_yes_no_and_wait_P(_T(MSG_RECOVER_PRINT), false) == LCD_LEFT_BUTTON_CHOICE) {
-              recover_print(0); 
-          } else { 
+          const uint8_t btn = lcd_show_fullscreen_message_yes_no_and_wait_P(_T(MSG_RECOVER_PRINT), false);
+          if ( btn == LCD_LEFT_BUTTON_CHOICE) {
+              recover_print(0);
+          } else { // LCD_MIDDLE_BUTTON_CHOICE
               eeprom_update_byte((uint8_t*)EEPROM_UVLO, PowerPanic::NO_PENDING_RECOVERY); 
-              lcd_update_enable(true); 
-              lcd_update(2); 
               lcd_setstatuspgm(MSG_WELCOME); 
           } 
       }
--- Firmware/power_panic.cpp
+++ Firmware/power_panic.cpp
@@ -316,8 +316,6 @@ ISR(INT4_vect) {
 }
 
 void recover_print(uint8_t automatic) {
-    lcd_update_enable(true);
-    lcd_update(2);
     lcd_setstatuspgm(_i("Recovering print"));////MSG_RECOVERING_PRINT c=20
 
     // Recover position, temperatures and extrude_multipliers
