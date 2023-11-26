9541d8429e0be10ed6d1c78f04ae7aed3ddae602
b <b@b3k.us>
2023-11-27 10:33:59 +1300

    Update thermal model for e3d Revo HT 40W based on actual measurement

--- Firmware/thermal_model/e3d_REVO_HT_40W.h
+++ Firmware/thermal_model/e3d_REVO_HT_40W.h
@@ -2,7 +2,7 @@
 
 #define THERMAL_MODEL_E3D_REVO_HT_40W_VER 1      // model parameters version
 
-#define THERMAL_MODEL_E3D_REVO_HT_40W_P 40.      // heater power (W)
+#define THERMAL_MODEL_E3D_REVO_HT_40W_P 38.      // heater power (W)
 #define THERMAL_MODEL_E3D_REVO_HT_40W_U 0.  // linear temperature coefficient (W/K/power)
 #define THERMAL_MODEL_E3D_REVO_HT_40W_V 1.     // linear temperature intercept (W/power)
 
@@ -10,10 +10,10 @@
 #define THERMAL_MODEL_E3D_REVO_HT_40W_R 20.5     // initial guess for heatblock resistance (K/W)
 
 #define THERMAL_MODEL_E3D_REVO_HT_40W_fS 0.065    // sim. 1st order IIR filter factor (f=100/27)
-#define THERMAL_MODEL_E3D_REVO_HT_40W_LAG 1600    // sim. response lag (ms, 0-2160)
+#define THERMAL_MODEL_E3D_REVO_HT_40W_LAG 2160    // sim. response lag (ms, 0-2160)
 
 #define THERMAL_MODEL_E3D_REVO_HT_40W_W 1.20     // Default warning threshold (K/s)
 #define THERMAL_MODEL_E3D_REVO_HT_40W_E 1.74     // Default error threshold (K/s)
 
 // fall-back resistance vector (R0-15)
-#define THERMAL_MODEL_E3D_REVO_HT_40W_Rv {THERMAL_MODEL_E3D_REVO_HT_40W_R, 18.4, 16.7, 15.2, 14.1, 13.3, 12.7, 12.1, 11.7, 11.3, 11., 10.8, 10.6, 10.4, 10.2, 10.1}
+#define THERMAL_MODEL_E3D_REVO_HT_40W_Rv {THERMAL_MODEL_E3D_REVO_HT_40W_R, 19.3, 17.7, 14.5, 13.6, 12.8, 11.9, 11., 10.8, 10.5, 10.1, 9.8, 9.6, 9.4, 9.2, 9.}
