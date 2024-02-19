2df8a18fde4904e6865ee676f9b83c60d2d97f0a
3d-gussner <3d.gussner@gmail.com>
2024-02-19 18:04:23 +0100

    Fix MK2.5/S builds

--- Firmware/Marlin_main.cpp
+++ Firmware/Marlin_main.cpp
@@ -8327,6 +8327,7 @@ Sigma_Exit:
     }
     break;
 
+#ifdef TMC2130
 #ifdef TMC2130_SERVICE_CODES_M910_M918
 
     /*!
@@ -8426,7 +8427,6 @@ Sigma_Exit:
     - `R` - Revert to EEPROM value
     - `Q` - Print effective silent/normal status. (Does not report override)
     */
-#ifdef TMC2130
     case 914:
     case 915:
     {
@@ -8465,7 +8465,6 @@ Sigma_Exit:
     }
     break;
 
-#endif // TMC2130
 #ifdef TMC2130_SERVICE_CODES_M910_M918
     /*!
     ### M916 - Set TMC2130 Stallguard sensitivity threshold <a href="https://reprap.org/wiki/G-code#M916:_Set_TMC2130_Stallguard_sensitivity_threshold">M916: Set TMC2130 Stallguard sensitivity threshold</a>
@@ -8538,6 +8537,7 @@ Sigma_Exit:
     break;
 
 #endif //TMC2130_SERVICE_CODES_M910_M918
+#endif // TMC2130
 
     /*!
 	### M350 - Set microstepping mode <a href="https://reprap.org/wiki/G-code#M350:_Set_microstepping_mode">M350: Set microstepping mode</a>
