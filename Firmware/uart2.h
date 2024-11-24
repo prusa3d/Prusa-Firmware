78d999a30057bda8dc16afac871c198061e1a092
gudnimg <gudni.m.g@gmail.com>
2024-11-24 09:28:26 +0000

    Convert uart2.c to a C++ file

--- Firmware/uart2.h
+++ Firmware/uart2.h
@@ -5,19 +5,9 @@
 #include <inttypes.h>
 #include <stdio.h>
 
-
-#if defined(__cplusplus)
-extern "C" {
-#endif //defined(__cplusplus)
-
-
 extern FILE _uart2io;
 #define uart2io (&_uart2io)
 
+void uart2_init(uint32_t baudRate);
 
-extern void uart2_init(uint32_t baudRate);
-
-#if defined(__cplusplus)
-}
-#endif //defined(__cplusplus)
 #endif //_UART2_H
